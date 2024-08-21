a38f7d01eb8bcf4e9ac27204daaaf1e36793a88f
Harsh <harshchamola@gmail.com>
2024-08-21 23:52:10 +0400

    final

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv').config();
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path');
 
 //databse connection
 mongoose.connect(process.env.MONGO_URL, {
@@ -11,12 +12,20 @@ mongoose.connect(process.env.MONGO_URL, {
 .then(() => console.log("database connected"))
 .catch((err) => console.log('database not connected', err))
 
+
 app.use(express.json());
 app.use(cors());
+
 
 //
 app.use('/', require('./routes/authRoutes')); // authentication routes
 app.use('/api/listing', require('./routes/listingRoutes')); //listing routes
+
+
+app.use(express.static(path.join(__dirname, "../client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
+});
 
 //running the server on port 8000
 const port = 8000;
